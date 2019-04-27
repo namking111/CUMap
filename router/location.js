d4e51d6777503d3fc3bf15407cbc7012b42e4f76
namking111 <namking_achiraya@hotmail.com>
2019-04-28 03:14:21 +0700

    :sparkles: Add description to landmark markers

--- router/location.js
+++ router/location.js
@@ -16,7 +16,7 @@ const router = require('express').Router();
 router.get('/atm', async (req, res) => {
     try {
         const connection = await getConnection();
-        const [atms] = await connection.query('SELECT Latitude, Longitude FROM ATM A JOIN Position P ON A.Landmark_id = P.Landmark_id');
+        const [atms] = await connection.query('SELECT Bank_name, Latitude, Longitude FROM ATM A JOIN Position P ON A.Landmark_id = P.Landmark_id');
         // console.log(locations[0].Landmark_id);
         res.status(200).send(atms);
     } catch (error) {
@@ -27,7 +27,7 @@ router.get('/atm', async (req, res) => {
 router.get('/canteen', async (req, res) => {
     try {
         const connection = await getConnection();
-        const [canteens] = await connection.query('SELECT Latitude, Longitude FROM Canteen C JOIN Position P ON C.Landmark_id = P.Landmark_id');
+        const [canteens] = await connection.query('SELECT Canteen_name, Latitude, Longitude FROM Canteen C JOIN Position P ON C.Landmark_id = P.Landmark_id');
         res.status(200).send(canteens);
     } catch (error) {
         res.status(500).send(error);
@@ -37,7 +37,7 @@ router.get('/canteen', async (req, res) => {
 router.get('/museum', async (req, res) => {
     try {
         const connection = await getConnection();
-        const [museums] = await connection.query('SELECT Latitude, Longitude FROM Museum M JOIN Position P ON M.Landmark_id = P.Landmark_id');
+        const [museums] = await connection.query('SELECT Museum_name, Latitude, Longitude FROM Museum M JOIN Position P ON M.Landmark_id = P.Landmark_id');
         res.status(200).send(museums);
     } catch (error) {
         res.status(500).send(error);
@@ -47,7 +47,7 @@ router.get('/museum', async (req, res) => {
 router.get('/coffeeshop', async (req, res) => {
     try {
         const connection = await getConnection();
-        const [coffeeshops] = await connection.query('SELECT Latitude, Longitude FROM Coffee_shop C JOIN Position P ON C.Landmark_id = P.Landmark_id');
+        const [coffeeshops] = await connection.query('SELECT Coffee_shop_name, Latitude, Longitude FROM Coffee_shop C JOIN Position P ON C.Landmark_id = P.Landmark_id');
         res.status(200).send(coffeeshops);
     } catch (error) {
         res.status(500).send(error);
@@ -57,7 +57,7 @@ router.get('/coffeeshop', async (req, res) => {
 router.get('/copyprint', async (req, res) => {
     try {
         const connection = await getConnection();
-        const [copyprints] = await connection.query('SELECT Latitude, Longitude FROM Copy_Print_shop C JOIN Position P ON C.Landmark_id = P.Landmark_id');
+        const [copyprints] = await connection.query('SELECT Copy_Print_shop_name, Latitude, Longitude FROM Copy_Print_shop C JOIN Position P ON C.Landmark_id = P.Landmark_id');
         res.status(200).send(copyprints);
     } catch (error) {
         res.status(500).send(error);
@@ -67,7 +67,7 @@ router.get('/copyprint', async (req, res) => {
 router.get('/vending_machine', async (req, res) => {
     try {
         const connection = await getConnection();
-        const [vendms] = await connection.query('SELECT Latitude, Longitude FROM Vending_Machine V JOIN Position P ON V.Landmark_id = P.Landmark_id');
+        const [vendms] = await connection.query('SELECT Vending_Machine_type, Latitude, Longitude FROM Vending_Machine V JOIN Position P ON V.Landmark_id = P.Landmark_id');
         res.status(200).send(vendms);
     } catch (error) {
         res.status(500).send(error);
@@ -77,7 +77,7 @@ router.get('/vending_machine', async (req, res) => {
 router.get('/library', async (req, res) => {
     try {
         const connection = await getConnection();
-        const [libraries] = await connection.query('SELECT Latitude, Longitude FROM Library L JOIN Position P ON L.Landmark_id = P.Landmark_id');
+        const [libraries] = await connection.query('SELECT Library_name, Latitude, Longitude FROM Library L JOIN Position P ON L.Landmark_id = P.Landmark_id');
         res.status(200).send(libraries);
     } catch (error) {
         res.status(500).send(error);
